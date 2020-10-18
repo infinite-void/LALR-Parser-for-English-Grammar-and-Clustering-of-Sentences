77b03bae0300f23389f72b1be65de92c2d52d3af
infinite-void <srinathsk18@gmail.com>
2020-10-18 16:43:41 +0530

    final

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Console } = require("console");
+//const { Console } = require("console");
 const express = require("express");
 const app = express();
 //const spawn = require("child_process").spawn;
 const spawnSync = require("child_process").spawnSync;
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
@@ -20,30 +21,19 @@ app.get("/", (req, res, next) => {
 	var clusterData, posData;
 	var reply = {};
 	console.log("Input   :  " + input + "\n\n");
-
 	
 	const proPOS = spawnSync('python3', ["./postagger.py", input], { encoding : 'utf8' });
 	posData = proPOS.output;
 
 	const proCtr = spawnSync('python3', ["./cluster.py", input], { encoding : 'utf8' });
 	clusterData = proCtr.output;
-	/*const pythonProcessPOS = spawn('python3',["./postagger.py", input]);
-	pythonProcessPOS.stdout.on('data', (data) => {
-		console.log("POS tag   : " + data.toString() + "\n\n");
-		posData = data.toString();
-	});
-	
-	const pythonProcess = spawn('python3',["./cluster.py", input]);
-	pythonProcess.stdout.on('data', (data) => {
-		console.log("Cluster Part   : \n" + data.toString() + "\n\n");
-		clusterData = data.toString();	
-	});*/
 	
 	reply["pos"] = posData;
 	reply["cluster"] = clusterData;
-	res.json(reply);
-});
 
+	res.json(reply); 
+});
+ 
 
 app.listen(3000, () => {
  	console.log("Server running on port 3000");
